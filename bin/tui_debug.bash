cfa9f403924e287e9aa3a3ca4210cf39c5e0e250
Dario Cangialosi @Arkenidar <dario.cangialosi@gmail.com>
2025-12-20 19:26:44 +0100

    Add TUI debug script for VSCode LSP integration

---
+++ bin/tui_debug.bash
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# A simple TUI debug script for use with LSP from VSCode.
+rdbg --open --port 12345 -- bin/tui
+
+# For VSCode : Ruby Extensions Pack
+# https://marketplace.visualstudio.com/items?itemName=Shopify.ruby-extensions-pack
