d8e3f2ffce47ad7df3a4505140eaabe52af0b4a1
Dario Cangialosi @Arkenidar <dario.cangialosi@gmail.com>
2025-12-20 13:02:10 +0100

    [debug] [vscode] [lsp] Add VS Code launch configuration and debugging documentation
    - Create .vscode/launch.json for Ruby debugging configurations
    - Add my_bash_tui_debug.cmd for launching TUI with debugger
    - Include vscode_lsp_ruby_debug_attach.txt for debugging instructions
    - Update .gitignore to comment out .vscode directory
    - Add screenshot for debugging setup

---
+++ bin/my_bash_tui_debug.cmd
@@ -0,0 +1,7 @@
+
+cd C:\Ruby34-x64\msys64\home\dario\database
+
+set MSYSTEM=MINGW64
+set CHERE_INVOKING=1
+set MSYS2_PATH_TYPE=inherit
+start C:\Ruby34-x64\msys64\usr\bin\bash.exe --login -i -c "rdbg --open --port 12345 -- bin/tui"
